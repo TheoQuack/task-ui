54ebd28d0d2f6cc704f18ce5ba706d1196f8ec4f
Theo <ranalan.theo123@gmail.com>
2025-07-23 01:09:27 +0800

    changed alias to Task list

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Registration from './components/Registeration';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './components/HomePage';
 import AddTaskForm from './components/AddTaskForm';
-import EnhancedTable from './components/TaskList';
+import TaskList from './components/TaskList';
 import { AuthProvider } from './context/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
 import ProfileDashboard from './components/ProfileDashboard';
@@ -18,7 +18,7 @@ function App() {
         <Route path="/" element={<PrivateRoute><Home/></PrivateRoute>}></Route>
         <Route path="/registration" element={<Registration/>}></Route>
         <Route path="/login" element={<LoginForm  />}></Route>
-        <Route path="/tasks" element={<PrivateRoute><EnhancedTable/></PrivateRoute>}></Route>
+        <Route path="/tasks" element={<PrivateRoute><TaskList/></PrivateRoute>}></Route>
         <Route path='/newtask' element={<PrivateRoute><AddTaskForm/></PrivateRoute>}></Route>
         <Route path='/profile' element={<PrivateRoute><ProfileDashboard/></PrivateRoute>}></Route>
       </Routes>
